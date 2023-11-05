a3e52a4ded70543e44c8c161d239be238e90c087
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2023-11-05 22:59:28 +0500

    Service Section Complete

--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -1,34 +1,87 @@
 import React from "react";
+import { FaCode } from "react-icons/fa";
 import {
-  FaCode,
-  FaHtml5,
-  FaCss3Alt,
-  FaReact,
-  FaNode,
-  FaPhp,
-} from "react-icons/fa";
-import { SiTailwindcss, SiMongodb, SiExpress, SiJquery } from "react-icons/si";
-import { WEB_DEVELOPMENT } from "../utils/contants";
+  AiOutlineRobot,
+  AiOutlineHighlight,
+  AiOutlineDesktop,
+} from "react-icons/ai";
+import { BsRobot } from "react-icons/bs";
+
+const ServiceCard = ({ icon, title, description }) => {
+  return (
+    <>
+      <div className="flex flex-col col-span-4 shadow-xl  p-5 transition-all  group hover:bg-turquoise rounded-md border">
+        <p className="text-5xl text-turquoise opacity-70 group-hover:text-white">
+          {icon}
+        </p>
+        <h1 className="font-formula text-2xl text-turquoise group-hover:text-white">
+          {title}
+        </h1>
+        <p className="group-hover:text-white opacity-60 font-sans">
+          {description}
+        </p>
+      </div>
+    </>
+  );
+};
 
 const ServiceSection = () => {
   const servicesName = [
-    "Deep Learning",
-    "Machine Learnig",
-    "Web Development",
-    "Desktop Application",
+    {
+      title: "Web Design",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      icon: <AiOutlineHighlight />,
+    },
+    {
+      title: "Web Development",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      icon: <FaCode />,
+    },
+    {
+      title: "Desktop Application",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      icon: <AiOutlineDesktop />,
+    },
+    {
+      title: "Deep Learning",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      icon: <AiOutlineRobot />,
+    },
+    {
+      title: "Machine Learning",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      icon: <BsRobot />,
+    },
+    {
+      title: "Machine Learning",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      icon: <BsRobot />,
+    },
   ];
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col justify-center">
       <div className="w-full flex justify-center items-center">
         <h1 className="absolute opacity-10 w-full text-center text-9xl font-formula text-turquoise">
-          SERVICES
+          SERVICES.
         </h1>
         <div className="w-full flex flex-col justify-center items-center mt-28">
           <p className="text-lg">WHAT I DO!</p>
-          <h1 className="font-formula text-4xl text-turquoise">My Services</h1>
+          <h1 className="font-formula text-5xl text-turquoise">Services</h1>
         </div>
       </div>
-      <div className="flex justify-end w-full"></div>
+      <div className="flex justify-center">
+        <div className="w-[80%] grid grid-cols-12 gap-6">
+          {servicesName.map((service, index) => {
+            return <ServiceCard {...service} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
