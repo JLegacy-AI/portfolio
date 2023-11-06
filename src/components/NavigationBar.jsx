17f6b9e250b24c4418f611e4a0d75334a0ac8c02
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2023-11-06 18:47:28 +0500

    Smooth Scrolling Added

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,25 +1,76 @@
 import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 const NavigationBar = () => {
   return (
-    <div className="absolute z-10 top-0 left-0 right-0 w-full h-16 flex justify-between items-center">
+    <div
+      id="home"
+      className="absolute z-10 top-0 left-0 right-0 w-full h-16 flex justify-between items-center"
+    >
       <h1 className="font-formula text-3xl text-turquoise w-1/2 text-center">
         Jamal.
       </h1>
       <ul className="flex justify-center items-center gap-8 w-1/2 text-white ">
         <li>
-          <a href="#about">About</a>
+          <Link
+            className="cursor-pointer"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            About
+          </Link>
         </li>
         <li>
-          <a href="#service">Services</a>
+          <Link
+            className="cursor-pointer"
+            to="service"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            Services
+          </Link>
         </li>
         <li>
-          <a href="#project">Projects</a>
+          <Link
+            className="cursor-pointer"
+            to="project"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            Project
+          </Link>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <Link
+            className="cursor-pointer"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            Contact
+          </Link>
         </li>
       </ul>
+      <Link
+        className="fixed bottom-6 right-6 cursor-pointer w-[40px] h-[40px] bg-white text-turquoise rounded-md shadow-2xl shadow-turquoise text-xl flex justify-center items-center hover:bg-turquoise hover:text-white"
+        to="home"
+        spy={true}
+        smooth={true}
+        offset={50}
+        duration={500}
+      >
+        <AiOutlineArrowUp />
+      </Link>
     </div>
   );
 };
